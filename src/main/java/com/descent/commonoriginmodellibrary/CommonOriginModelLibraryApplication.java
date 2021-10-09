0296df49ac2e486c669e27d60bc0b7348856e586
karan <karan.saj@gmail.com>
2021-10-09 22:51:47 +0530

    initial project setup in gradle

---
+++ src/main/java/com/descent/commonoriginmodellibrary/CommonOriginModelLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.descent.commonoriginmodellibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonOriginModelLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonOriginModelLibraryApplication.class, args);
+	}
+
+}
